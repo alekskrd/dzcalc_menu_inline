1e4fc440e91e82ee6c3b0d1a848b5e1e773925ee
alekskrd <als.d@mail.ru>
2023-01-06 15:10:46 +0300

    Добавил меню и кнопки инлайн

---
+++ lexicon/lexicon_ru.py
@@ -0,0 +1,9 @@
+LEXICON_RU: dict[str, str] = {'/contact': 'https://t.me/alekskrd_krd',
+                              '/calculate': f'''Выберите вид калькуляторов:
+      Комплексные числа: /complex вид выражения при вводе -> a+bj + c-dj
+      Дробные числа: /fraction вид выражения при вводе -> a/b + c/d
+      Просмотр лога вычислений: /calc_log''',
+                              '/calc': 'Введи выражение'
+                              }
+
+
--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import logging
-from aiogram import Bot, types, executor
-from aiogram.dispatcher import Dispatcher
+from aiogram import Bot, types, executor, Dispatcher
 from aiogram.dispatcher.filters import Command
-
 from calc_complex import calc_complex_main
 from calc_fraction import calc_fraction_main
 from calc_log_reader import log_reader
 from config import TOKEN_API
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 
+from lexicon.lexicon_ru import LEXICON_RU
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
+bot: Bot = Bot(TOKEN_API)
+dp: Dispatcher = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+async def start_command(message: Message):
     await message.reply("Привет!\nЯ пока что умею вычислять комплексные и вещественные числа!\n"
-                        "Для вывода списка комманд введите /help")
-    await message.delete()
+                        "Для вывода списка команд введите /help",
+                        reply_markup=keyboard)
+
+
+# Создаем объект инлайн-клавиатуры
+keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup()
+
+# Создаем объекты инлайн-кнопок
+button_1: InlineKeyboardButton = InlineKeyboardButton(
+    text='комплексные вычисления',
+    callback_data='big_button_1_pressed')
+
+button_2: InlineKeyboardButton = InlineKeyboardButton(
+    text='рациональные вычисления',
+    callback_data='big_button_2_pressed')
+
+
+# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# с data 'big_button_1_pressed'
+@dp.callback_query_handler(text='big_button_1_pressed')
+async def process_button_1_press(callback: CallbackQuery):
+    await callback.message.edit_text(text='Введите /complex далее что нужно подсчитать',
+                                     reply_markup=callback.message.reply_markup)
+
+
+# Этот хэндлер будет срабатывать на апдейт типа CallbackQuery
+# с data 'big_button_2_pressed'
+@dp.callback_query_handler(text='big_button_2_pressed')
+async def process_button_2_press(callback: CallbackQuery):
+    await callback.message.edit_text(text='Введите /fraction далее что нужно подсчитать',
+                                     reply_markup=callback.message.reply_markup, )
+    await callback.answer()
+    # await message.reply(f" {calc_fraction_main(command.args)}")
+
+
+# Добавляем кнопки в клавиатуру методом add
+keyboard.add(button_1).add(button_2)
 
 
 @dp.message_handler(commands=['help'])
@@ -27,13 +62,9 @@ async def help_command(message: types.Message):
     await message.reply('''Калькулятор: /calculate\n ''')
 
 
-#
 @dp.message_handler(commands=['calculate'])
 async def calc_command(message: types.Message):
-    await message.reply(f'''Выберите вид калькуляторов:
-      Комплексные числа: /complex вид выражения при вводе -> a+bj + c-dj
-      Дробные числа: /fraction вид выражения при вводе -> a/b + c/d
-      Просмотр лога вычислений: /calc_log''')
+    await message.answer(text=LEXICON_RU['/calculate'])
 
 
 @dp.message_handler(commands=['complex'])
@@ -51,5 +82,22 @@ async def log_command(message: types.Message):
     await message.reply(log_reader())
 
 
+@dp.message_handler(commands=['contact'])
+async def process_contact(message: Message):
+    await message.answer(text=LEXICON_RU['/contact'])
+
+
+# Создаем асинхронную функцию
+async def set_main_menu(dp: Dispatcher):
+    # Создаем список с командами для кнопки menu
+    main_menu_commands = [
+        types.BotCommand(command='/help', description='Справка по работе калькулятора'),
+        types.BotCommand(command='/contact', description='Способы связи'),
+        types.BotCommand(command='/calculate', description='Калькулятор')
+
+    ]
+    await dp.bot.set_my_commands(main_menu_commands)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True, on_startup=set_main_menu)
